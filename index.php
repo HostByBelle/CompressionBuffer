26190518e8134584ab2c3db54b1ead88bec2f7f5
Belle Aerni <belleaerni@gmail.com>
2024-04-06 20:41:02 -0700

    Added a screenshot showing zstd on the PHP dev server

--- index.php
+++ index.php
@@ -6,35 +6,7 @@ require 'vendor/autoload.php';
 
 CompressionBuffer::setUp();
 ob_start(CompressionBuffer::handler(...));
-echo "test";
-echo "test";
-echo "test";
-echo "test";
-echo "test";
-echo "test";
-echo "test";
-echo "test";
-echo "test";
-echo "test";
-echo "test";
-echo "test";
-echo "test";
-echo "test";
-echo "test";
-echo "test";
-echo "test";
-echo "test";
-echo "test";
-echo "test";
-echo "test";
-echo "test";
-echo "test";
-echo "test";
 
-//ob_get_flush();
-//ob_get_clean();
+echo "If you can read this, it works!";
+
 ob_end_flush();
-//ob_end_clean();
-
-//
-//print_r($encodings);
