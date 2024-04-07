9a4b977119a82aff468421b17c7228b4d372b417
Belle Aerni <belleaerni@gmail.com>
2024-04-06 18:19:45 -0700

    Did the benchmarking, created a starter readme

--- tests/compressionTest.php
+++ tests/compressionTest.php
@@ -1,25 +1,76 @@
 <?php
-function compressWithGzip($data)
+function compressWithGzip(string $data, int $level)
 {
-    return gzencode($data, 9, ZLIB_ENCODING_GZIP);
+    return gzencode($data, $level, ZLIB_ENCODING_GZIP);
 }
 
-function compressionTest()
+function compressWithZstd(string $data, int $level)
 {
-    $file = 'war_and_peace.html';
+    return zstd_compress($data, $level);
+}
 
-    // Read file content
-    $originalData = file_get_contents($file);
+function compressWithBrotli(string $data, int $level)
+{
+    return brotli_compress($data, $level);
+}
+
+function testGzip()
+{
+    $level = 1;
+    $max = 9;
+    $originalData = file_get_contents('war_and_peace.html');
     $originalLength = strlen($originalData);
 
-    echo "Original data size: " . strlen($originalData) . "\n";
-
-    // Compress using gzip
-    $gzipSize = strlen(compressWithGzip($originalData));
-    $ratio = 100 - round(($gzipSize / $originalLength) * 100, 3);
-
-    echo "GZIP size reduction: " . $ratio . "%\n";
+    echo '----GZIP----' . PHP_EOL;
+    echo 'Level, Reduction, Time, Percent Per Millisecond' . PHP_EOL;
+    for ($level; $level <= $max; $level++) {
+        $start = hrtime(true);
+        $newSize = strlen(compressWithGzip($originalData, $level));
+        $ratio = 100 - round(($newSize / $originalLength) * 100, 3);
+        $time = (hrtime(true) - $start) / 1e+6;
+        $percentPerMs = $ratio / $time;
+        echo "$level, $ratio%, $time ms, $percentPerMs% per milisecond" . PHP_EOL;
+    }
 }
 
-compressionTest();
+function testZstd()
+{
+    $level = 1;
+    $max = 19;
+    $originalData = file_get_contents('war_and_peace.html');
+    $originalLength = strlen($originalData);
 
+    echo '----zstd----' . PHP_EOL;
+    echo 'Level, Reduction, Time, Percent Per Millisecond' . PHP_EOL;
+    for ($level; $level <= $max; $level++) {
+        $start = hrtime(true);
+        $newSize = strlen(compressWithZstd($originalData, $level));
+        $ratio = 100 - round(($newSize / $originalLength) * 100, 3);
+        $time = (hrtime(true) - $start) / 1e+6;
+        $percentPerMs = $ratio / $time;
+        echo "$level, $ratio%, $time ms, $percentPerMs% per milisecond" . PHP_EOL;
+    }
+}
+
+function testBrotli()
+{
+    $level = 1;
+    $max = 11;
+    $originalData = file_get_contents('war_and_peace.html');
+    $originalLength = strlen($originalData);
+
+    echo '----Brotli----' . PHP_EOL;
+    echo 'Level, Reduction, Time, Percent Per Millisecond' . PHP_EOL;
+    for ($level; $level <= $max; $level++) {
+        $start = hrtime(true);
+        $newSize = strlen(compressWithBrotli($originalData, $level));
+        $ratio = 100 - round(($newSize / $originalLength) * 100, 3);
+        $time = (hrtime(true) - $start) / 1e+6;
+        $percentPerMs = $ratio / $time;
+        echo "$level, $ratio%, $time ms, $percentPerMs% per milisecond" . PHP_EOL;
+    }
+}
+
+testGzip();
+testZstd();
+testBrotli();
